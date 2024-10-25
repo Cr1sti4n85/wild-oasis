a872d57a9dd788df2d32c30e12aa6943e7e13fbf
Cristian Perez <cperezlecaros@gmail.com>
2024-10-25 16:14:43 -0700

    Install react hot toast and apply to success/error messages

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Cabins from "./pages/Cabins";
 import Bookings from "./pages/Bookings";
 import AppLayout from "./ui/AppLayout";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -42,6 +43,26 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 4000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -59,9 +60,9 @@ function CabinRow({ cabin }) {
     onSuccess: () => {
       //invalidate cache:
       queryClient.invalidateQueries({ queryKey: ["cabins"] });
-      alert("Cabin succesfully deleted");
+      toast.success("Cabin succesfully deleted");
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
 
   return (
