1db91ad82accc74d7605a1745b6feca1a7927dde
Cristian Perez <cperezlecaros@gmail.com>
2024-10-27 10:06:54 -0700

    Add upload cabin image functionality

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://hilvtiyekzrqmjajtxzd.supabase.co";
+export const supabaseUrl = "https://hilvtiyekzrqmjajtxzd.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpbHZ0aXlla3pycW1qYWp0eHpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk3MzE1MDMsImV4cCI6MjA0NTMwNzUwM30.yePFGHkk9OmDOkZS2EHqiss2UE0lIzcds5rmukvlMbk";
 const supabase = createClient(supabaseUrl, supabaseKey);
