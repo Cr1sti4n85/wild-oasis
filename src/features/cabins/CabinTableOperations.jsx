7b5c0adb59214a93a9ed68e1fea42987fb0b77da
Cristian Perez <cperezlecaros@gmail.com>
2024-11-04 09:18:51 -0700

    Build Select and SortBy component

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,5 +1,6 @@
 import TableOperations from "../../ui/TableOperations";
 import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
 
 function CabinTableOperations() {
   return (
@@ -10,6 +11,16 @@ function CabinTableOperations() {
           { value: "all", label: "All" },
           { value: "no-discount", label: "No Discount" },
           { value: "with-discount", label: "With Discount" },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: "name_asc", label: "By name [A-Z]" },
+          { value: "name-desc", label: "By name [Z-A]" },
+          { value: "regularPrice-asc", label: "By price (asc.)" },
+          { value: "regularPrice-desc", label: "By price (desc.)" },
+          { value: "maxCapacity-asc", label: "By capacity (asc.)" },
+          { value: "maxCapacity-desc", label: "By capacity (desc.)" },
         ]}
       />
     </TableOperations>
