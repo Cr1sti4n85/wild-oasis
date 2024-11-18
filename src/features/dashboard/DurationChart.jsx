e90390ddb0eebd7fd7e5c9ad826c5017cdc917bd
Cristian Perez <cperezlecaros@gmail.com>
2024-11-17 17:04:39 -0700

    Implement displaying of today's arrivals and departures

--- src/features/dashboard/DurationChart.jsx
+++ src/features/dashboard/DurationChart.jsx
@@ -145,7 +145,7 @@ function DurationChart({ confirmedStays }) {
   const { isDarkMode } = useDarkMode();
   const startData = isDarkMode ? startDataDark : startDataLight;
   const data = prepareData(startData, confirmedStays);
-
+  console.log(data);
   return (
     <ChartBox>
       <Heading as="h2">Stay Duration Summary</Heading>
